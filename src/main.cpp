f8cc34dcb9a1b3cf54542f00f7d7192ba8a7c5ed
Julian Lacey <Julianmlacey@gmail.com>
2025-04-13 09:51:10 -0700

    working retrieval and back to home

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <debug.h>
 #include <Arduino.h> // This is needed for the ESP32 to work with base Arduino libraries
+#include "debug.h"
 #include "common.h"
 #include "dc_motors.h"
 #include "servo_motors.h"
 #include "communications.h"
 #include "stdint.h"
+#include "MedianFilterLib.h"
+#include "PID_v1.h"
 
-#define myID 5
-#define TEAMCOL 3
-
+#define myID 5 //Robot ID
+#define TEAMCOL 3 //Team Cylinder Color
+#define SPEEDLIMIT 100 //Speed Limit for the motors as a percent
+#define HOMEX 220 //Home location
+#define HOMEY 220 //Home location
+#define BUFFLENGTH 5//Buffer Length for Median Filter
+#define IRSENSORPIN 36  // IR Sensor Pin
+#define IRSENSORENABLE 10 // IR Sensor Enable Pin
+#define NUMBALLS 3 // Number of balls in the field
+#define pinSW 21 //limit switch pin
 //----------------------------------FUNCS----------------------------------
-int goToLoction();
+
 void hunting();
-void open();
-void close();
 void defending();
 void capturing();
+void goToHome();
+int goToLoction();
+double* transformRobotPos(uint16_t X, uint16_t Y);
+void open(Servo* clawservo);
+void close(Servo* clawservo);
 
 //----------------------------------VARS----------------------------------
 /*
@@ -37,8 +50,8 @@ Servo clawServo;
 
 
 int ballNum; //number of balls/cylinders in the field
-int16_t x = 0;  //x location of robot in pixels
-int16_t y = 0;  //y location of robot in pixels
+double x = 0;  //x location of robot in pixels
+double y = 0;  //y location of robot in pixels
 double theta = 0; //rotation of robot in radians
 /*
 omega1 & omega2: speed of the left and right motors from -100 - 0 - 100 (100 = full speed forward, -100 = full speed backward)
@@ -57,8 +70,8 @@ double omega_1, omega_2, trueOmega_1, trueOmega_2;
   Error theta: the difference between the robot's rotation and the vector between the robot and the target location
 */
 double error_x = 0;
-double  error_y = 0;
-double  error_d = 0;
+double error_y = 0;
+double error_d = 0;
 double error_theta = 0;
 
 
@@ -69,108 +82,171 @@ double error_theta = 0;
 double prev_error_d = 0;
 double prev_error_theta = 0;
 
-double Kp1 = 1;
-double Kd1 = 3;
+const double Kp1 = 1;//1
+const double Kd1 = 2.7;//1.1
 
-double Kp2 = 9;
-double Kd2 = 0.5;
+const double Kp2 = 19;//12
+const double Kd2 = 1.95;//1.5
+
+
+static uint8_t IRSensorValue = 0; // IR Sensor Value
+
+/*
+Median Filters for sensor values
+*/
+MedianFilter<double>robotXFilter(BUFFLENGTH);
+MedianFilter<double>robotYFilter(BUFFLENGTH);
+MedianFilter<double>robotThetaFilter(BUFFLENGTH);
+MedianFilter<double>IRSensorMagnitudeFilter(BUFFLENGTH);
+/*
+hw_timer_t* switchTimer = NULL;
+*/
 
 
 //-------------------SETUP----------------------------------
 void setup() {
 
+
+  //---------General Setup---------
   Serial.begin(115200); //begin serial communication at 115200 baud rate
   setupServos();      //setup the servos
   setupCommunications();  //setup the communications with the camera
 
-  //setup servos
+  //---------Drive Servo Setup---------
   servo3.attach(SERVO2_PIN, 1300, 1700);
   servo4.attach(SERVO1_PIN, 1300, 1700);
   clawServo.attach(SERVO3_PIN);
+
+
+  //---------Limit Switch Setup---------
+  //pinMode(IRSENSORENABLE, OUTPUT);
+  //pinMode(pinSW, INPUT);
+
+
+//---------Limit Switch Setup---------
+
+  delay(1000);
 }
 
+//void setupSwitchInterupt() {
+  //pinMode(pinSW, INPUT);
+  //attachInterrupt(digitalPinToInterrupt(pinSW), onSwitchInterupt, FALLING);
+//}
 //-------------------STATES----------------------------------
 
-void hunting() {
-  open();
-  int ballnum = 0;
-  BallPosition closestBall;
-
-  double minDist = INFINITY;
-
-  getBallPositions(currentBallPoss);
-
-  for (int i = 0; i < 3; i++) {
-    if (currentBallPoss[ i ].hue == TEAMCOL) {
-      int dx = currentBallPoss[ i ].x - x;
-      int dy = currentBallPoss[ i ].y - y;
-      int dist = sqrt(dx * dx + dy * dy);
-      if (dist < minDist) {
-        minDist = dist;
-        closestBall = currentBallPoss[ i ];
-        ballnum = i;
-      }
-    }
-  }
-
-
-  goToLoction();
-  //close();
-  //goToLoction(260, 260);
-  open();
-
-  while (1) {
-
-    Serial.println("finished");
-  }
-
-}
-
-/*
-  //go to the cylinder
-goToLoction(closestBall.x, closestBall.y);
-//pick up the cylinder
-pickupBlock();
-//go to home
-goToLoction(0, 0); //SET HOME LOCATION
-//open claw
-open();
-*/
-
+void hunting() {}
 void defending() {}
 void capturing() {}
 
-
 //Open the claw
-void open() {
-  clawServo.write(20);  //Figure out OPEN AND CLOSE VALUES*****
+void open(Servo* clawservo) {
+  clawservo->write(20);  //Figure out OPEN AND CLOSE VALUES*****
   delay(15);
   return;
 }
 
 //Close the claw
-void close() {
-  clawServo.write(85);
+void close(Servo* clawservo) {
+  clawservo->write(85);
   delay(15);
   return;
 }
 
+double* transformRobotPos(uint16_t X, uint16_t Y) {
+  static double robotPos[ 2 ];
+  //static double* robotPos = pos[1];
 
+  double m1 = 1.335, m2 = 1.33, b1 = 384.475, b2 = 328.15;
 
+  robotPos[ 0 ] = (m1 * X) - b1;
+  robotPos[ 1 ] = (m2 * Y) - b2;
 
-void pickupBlock() {
-  //open
-  //go to location
-  //close
-  //go to home
-  //open;
-
-
+  return robotPos;
 }
 
+void goToHome() {
+  error_d =
+    prev_error_d = 0;
+  prev_error_theta = 0;
+  bool isHome = false;
 
+  while (isHome == false) {
+    RobotPose myPose = getRobotPose(myID);
+    if (myPose.valid == true) {
+      double* robotPos = transformRobotPos(myPose.x, myPose.y);
+      x = robotXFilter.AddValue(robotPos[ 0 ]);
+      y = robotYFilter.AddValue(robotPos[ 1 ]);
+
+      theta = robotThetaFilter.AddValue(myPose.theta);
+
+
+    } else {
+      Serial.println("Invalid Pose");
+     // return;
+    }
+
+    double d_x = HOMEX; //home x location
+    double d_y = HOMEY; //home y location
+    error_x = d_x - x;
+    error_y = d_y - y;
+    error_d = sqrt(abs(error_x * error_x) + abs(error_y * error_y));
+    error_theta = (atan2(error_y, error_x) - (theta / 1000)) * (180 / (PI)) - 90;
+
+    if (error_theta < -180) {
+      error_theta = error_theta + 360;
+    } else if (error_theta > 180) {
+      error_theta = error_theta - 360;
+    }
+
+    //PID controller for left and right motor
+    omega_1 = 0.2 * (Kp1 * error_d - Kp2 * error_theta + Kd1 * (error_d - prev_error_d) - Kd2 * (error_theta - prev_error_theta));
+    omega_2 = 0.2 * (Kp1 * error_d + Kp2 * error_theta + Kd1 * (error_d - prev_error_d) + Kd2 * (error_theta - prev_error_theta));
+
+    //limit PID controller
+    omega_1 = constrain(omega_1, -SPEEDLIMIT, SPEEDLIMIT);
+    omega_2 = constrain(omega_2, -SPEEDLIMIT, SPEEDLIMIT);
+
+    trueOmega_1 = map(omega_1, -100, 100, 1300, 1700);
+    trueOmega_2 = map(-omega_2, -100, 100, 1300, 1700);
+
+    //write the microseconds to the servos
+    servo3.writeMicroseconds(trueOmega_1);
+    servo4.writeMicroseconds(trueOmega_2);
+
+    D_print(d_x);
+    D_print(" ");
+    D_print(d_y);
+    D_print("   ");
+    D_print(x);
+    D_print(" ");
+    D_print(y);
+    D_print("   ");
+    D_print(omega_1);
+    D_print(" ");
+    D_print(omega_2);
+    D_print("   ");
+    D_print(error_d);
+    D_print("   ");
+    D_println(error_theta);
+
+    if (error_d < 150) {
+      servo3.writeMicroseconds(1500);
+      servo4.writeMicroseconds(1500);
+      delay(1000);
+      open(&clawServo);
+      isHome = true;
+      while (1) {}
+      //return 1;
+    }
+    prev_error_d = error_d;
+    prev_error_theta = error_theta;
+
+  }
+  //return 0;
+}
 
 int goToLoction() {
+  open(&clawServo);
   BallPosition closestBall;
   RobotPose myPose = getRobotPose(myID);
   bool inReach = false;
@@ -192,18 +268,21 @@ int goToLoction() {
         }
       }
     }
+
     //if valid pose, update x, y, and theta to new location
     if (myPose.valid == true) {
-      x = myPose.x;
-      y = myPose.y;
+      double* robotPos = transformRobotPos(myPose.x, myPose.y);
+      x = robotPos[ 0 ];
+      y = robotPos[ 1 ];
+
       theta = myPose.theta;
     } else {
       Serial.println("Invalid Pose");
       return 1;
     }
 
-    int d_x = closestBall.x; //target x location
-    int d_y = closestBall.y; //target y location
+    double d_x = static_cast<double>(closestBall.x); //target x location
+    double d_y = static_cast<double>(closestBall.y); //target y location
 
     //calculate difference between robot and target location
     error_x = d_x - x;
@@ -225,8 +304,9 @@ int goToLoction() {
     omega_2 = 0.2 * (Kp1 * error_d + Kp2 * error_theta + Kd1 * (error_d - prev_error_d) + Kd2 * (error_theta - prev_error_theta));
 
     //limit PID controller
-    omega_1 = constrain(omega_1, -100, 100);
-    omega_2 = constrain(omega_2, -100, 100);
+    omega_1 = constrain(omega_1, -SPEEDLIMIT, SPEEDLIMIT);
+    omega_2 = constrain(omega_2, -SPEEDLIMIT, SPEEDLIMIT);
+
 
     //map omega1 and omega2 to microseconds for the motorshield
     trueOmega_1 = map(omega_1, -100, 100, 1300, 1700);
@@ -236,30 +316,31 @@ int goToLoction() {
     servo3.writeMicroseconds(trueOmega_1);
     servo4.writeMicroseconds(trueOmega_2);
 
-    Serial.print(d_x);
-    Serial.print(" ");
-    Serial.print(d_y);
-    Serial.print(" ");
-    Serial.print(x);
-    Serial.print(" ");
-    Serial.print(y);
-    Serial.print(" ");
-    Serial.print(omega_1);
-    Serial.print(" ");
-    Serial.print(omega_2);
-    Serial.print(" ");
-    Serial.print(error_d);
-    Serial.print(" ");
-    Serial.println(error_theta);
 
-    //if close enough to the target location, stop the motors
-    if (error_d < 100) {
+    D_print(d_x);
+    D_print(" ");
+    D_print(d_y);
+    D_print("   ");
+    D_print(x);
+    D_print(" ");
+    D_print(y);
+    D_print("   ");
+    D_print(omega_1);
+    D_print(" ");
+    D_print(omega_2);
+    D_print("   ");
+    D_print(error_d);
+    D_print("   ");
+    D_println(error_theta);
+
+  //if close enough to the target location, stop the motors
+    if (error_d < 130) {
       servo3.writeMicroseconds(1500);
       servo4.writeMicroseconds(1500);
-      delay(1000);
-      close();
+      delay(100);
+      close(&clawServo);
       inReach = true;
-      while (1) {}
+      //while (1) {}
     }
     prev_error_d = error_d;
     prev_error_theta = error_theta;
@@ -267,24 +348,8 @@ int goToLoction() {
   return 0;
 }
 
-
 void loop() {
-
-  //hunting();
-    //defending();
-    //capturing();
-
   goToLoction();
-  //pickupBlock();
-  //open();
-  //delay(1000);
-  //goToLoction(0, 1000);
-  //pickupBlock();
-  //open();
-  //delay(1000);
-  //goToLoction(0, -1000);
-  //pickupBlock();
-  //open();
-  //delay(1000);
+  goToHome();
 
 }
